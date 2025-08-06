b9b67006cee9a5b255fb82775407aa4b3662000f
QiYueYiya <pardofeliss@outlook.com>
2025-08-06 14:28:50 +0800

    change api server

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rustdesk.070107.xyz:21114".to_owned()
+    "https://rustdesk.qiyueyi.top:21114".to_owned()
 }
 
 #[inline]
