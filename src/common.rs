a0f36adf19a842fc1d2528b651be4bdd5875083b
QiYueYiya <pardofeliss@outlook.com>
2025-09-18 22:04:41 +0800

    修改 API 服务器

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rustdesk.qiyueyi.top".to_owned()
+    "https://rustdesk.qiyueyi.top:21113".to_owned()
 }
 
 #[inline]
